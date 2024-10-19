da84d813d46daa9cbdd331097f350dc5a4e8f3e0
adnan011-gif <adnankhanofficial105@gmail.com>
2024-10-19 09:43:27 +0000

    datatypes

---
+++ .devcontainer/01 basics/02_datatypes.js
@@ -0,0 +1,12 @@
+
+
+//number => 2 to power 53
+// bigint
+//I string =
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique console. Log (typeof undefined); // undefined
+console. log (typeof null); // object
+
+
