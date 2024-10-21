efa396782bd457a091189616bef33a64080a1145
adnan011-gif <adnankhanofficial105@gmail.com>
2024-10-21 18:25:19 +0000

    strings

---
+++ .devcontainer/01 basics/04_strings.js
@@ -0,0 +1,25 @@
+let name= new String("Adnan0p")
+console.log(name)
+console.log(name.__proto__);
+console.log(name.charAt(3))
+console.log(name.indexOf("n"));
+
+const newstring= name.slice(-2,7)
+const anotherString= name.substring(1,6)
+console.log(newstring)
+console.log(anotherString);
+
+const newstringOne= "  hitesh"
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+const url= "https:/adnan%11"
+console.log(url.replace("%","-"));
+
+let NewName="Adnan-0p"
+console.log(NewName.split("-"));
+
+
+
+
+
