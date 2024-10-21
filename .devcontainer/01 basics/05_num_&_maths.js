4c14ed5fa1b364778686f1ae430938bb88d11798
adnan011-gif <adnankhanofficial105@gmail.com>
2024-10-21 18:25:42 +0000

    maths and numbers

---
+++ .devcontainer/01 basics/05_num_&_maths.js
@@ -0,0 +1,13 @@
+const number= new Number(100)
+console.log(number);
+
+console.log(number.toFixed(2))
+
+const otherNumber= 100.123
+console.log(otherNumber.toPrecision(3));
+console.log(Math.round(5.1));
+
+console.log((Math.random()*10)+1);
+
+
+
