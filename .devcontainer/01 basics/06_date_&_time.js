e493257abec9c233c88f05b65b40eec90e6e60f1
adnan011-gif <adnankhanofficial105@gmail.com>
2024-10-22 19:56:23 +0000

    date and time

---
+++ .devcontainer/01 basics/06_date_&_time.js
@@ -0,0 +1,28 @@
+const date= new Date()
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.toLocaleTimeString());
+
+let myDate= new Date(2024, 09,21)
+console.log(myDate);
+
+let MyTimeStamp= Date.now()
+console.log(MyTimeStamp);
+console.log(myDate.getTime());
+
+let NewDate= new Date()
+console.log(NewDate.getDate())
+
+myDate.toLocaleString('default' {
+    weekday:"long"
+})
+console.log(myDate);
+
+
+
+
+
+
---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+let myArr=[1,2,3,4,5]
+console.log(myArr);
+
+//slice
+let myArr1=myArr.slice(1,4)
+console.log(myArr1);
+
+//spilce
+let myArr2=myArr.splice(1,4)
+console.log(myArr2);
+console.log(myArr);
+
+//general properties
+console.log(myArr.includes(6));
+
+myArr.push(6)
+console.log(myArr);
+
+
+
+
+
