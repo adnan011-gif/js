b47d68ff10ab7e98d83710d35947076a949f572c
adnan011-gif <adnankhanofficial105@gmail.com>
2024-10-24 15:20:18 +0000

    function

---
+++ .devcontainer/03_basics/functions.js
@@ -0,0 +1,24 @@
+//function to add two no.
+function sum(num1,num2){
+    return num1+num2
+
+}
+console.log(sum(1,3))
+
+
+
+function cartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(cartPrice(200,100,400))
+
+
+const user= {
+    username:"Adnan" ,
+    email: "adnan@bhai",
+}
+
+function calluser(anyobject){
+    return anyobject.username
+}
+console.log(calluser(user))
